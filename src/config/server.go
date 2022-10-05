8cb07b11bed2e132edac0a7999f7d1626bd9a8c8
HendraLatumeten <hendrawyt34@gmail.com.com>
2022-10-05 12:50:14 +0700

    new

--- src/config/server.go
+++ src/config/server.go
@@ -19,8 +19,7 @@ var ServeCmd = &cobra.Command{
 func server(cmd *cobra.Command, args []string) error {
 	if mainRoute, err := routers.New(); err == nil {
 
-		var addrs string = "0.0.0.0:8080"
-
+		var addrs string = "127.0.0.1:8080"
 		if pr := os.Getenv("PORT"); pr != "" {
 			addrs = ":" + pr
 		}
